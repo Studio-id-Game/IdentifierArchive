a65e0d4a36ac90e1e2ed450d9021bd526f40fdb8
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2024-05-05 00:57:29 +0900

    シリアライズをJsonにして、他必要なファイルも生成

--- IdentifierArchiveCore/Setting.cs
+++ IdentifierArchiveCore/Setting.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
+using Utf8Json;
 
 namespace IdentifierArchiveCore
 {
@@ -10,88 +12,28 @@ namespace IdentifierArchiveCore
             public const string READ = "read";
         }
 
-        readonly struct SettingData
+        public struct SettingData
         {
+            public static SettingData FromBytes(byte[] bytes)
+            {
+                return JsonSerializer.Deserialize<SettingData>(bytes);
+            }
+
             public SettingData()
             {
             }
 
-            public string WorkSpace { get; init; } = "作業フォルダ";
-            public string ZipCommand { get; init; } = "暗号圧縮コマンド";
-            public string UnzipCommand { get; init; } = "復号解凍コマンド";
-            public string UploadCommand { get; init; } = "アップロードコマンド";
-            public string DownloadCommand { get; init; } = "ダウンロードコマンド";
+            public string WorkSpace { get; set; } = "作業フォルダ";
+            public string ZipCommand { get; set; } = "暗号圧縮コマンド";
+            public string UnzipCommand { get; set; } = "復号解凍コマンド";
+            public string UploadCommand { get; set; } = "アップロードコマンド";
+            public string DownloadCommand { get; set; } = "ダウンロードコマンド";
 
-            public string ToText()
+            readonly public byte[] ToBytes()
             {
-                return 
-                    @$"// 作業フォルダ
-{nameof(WorkSpace)} = {WorkSpace}
-
-// 暗号圧縮コマンド
-{nameof(ZipCommand)} = {ZipCommand}
-
-// 復号解凍コマンド
-{nameof(UnzipCommand)} = {UnzipCommand}
-
-// アップロードコマンド
-{nameof(UploadCommand)} = {UploadCommand}
-
-// ダウンロードコマンド
-{nameof(DownloadCommand)} = {DownloadCommand}";
+                return JsonSerializer.PrettyPrintByteArray(JsonSerializer.Serialize(this));
             }
 
-            public static SettingData FromText(string text)
-            {
-                var workSpace = "";
-                var zipCommand = "";
-                var unzipCommand = "";
-                var uploadCommand = "";
-                var downloadCommand = "";
-
-                var lines = text.Split('\n');
-                foreach(var line in lines)
-                {
-                    if(string.IsNullOrEmpty(line) || line.StartsWith("//")) continue;
-
-                    var eqIndex = line.IndexOf('=');
-
-                    if(eqIndex == -1) continue;
-
-                    var id = line[..eqIndex].TrimStart(' ').TrimEnd(' ');
-                    var value = line[(eqIndex+1)..].TrimStart(' ').TrimEnd(' ');
-
-                    if (id == nameof(WorkSpace))
-                    {
-                        workSpace = value;
-                    }
-                    else if (id == nameof(ZipCommand))
-                    {
-                        zipCommand = value;
-                    }
-                    else if (id == nameof(UnzipCommand))
-                    {
-                        unzipCommand = value;
-                    }
-                    else if (id == nameof(DownloadCommand))
-                    {
-                        downloadCommand = value;
-                    }
-                    else if (id == nameof(UploadCommand))
-                    {
-                        uploadCommand= value;
-                    }
-                }
-
-                return new()
-                {
-                    WorkSpace = workSpace,
-                    ZipCommand = zipCommand,
-                    UnzipCommand = unzipCommand,
-                    UploadCommand = uploadCommand,
-                    DownloadCommand = downloadCommand,
-                };
-            }
         }
 
         public override string Command => Commands.SETTING;
@@ -110,69 +52,88 @@ namespace IdentifierArchiveCore
             var subCommnad = args[0];
             var directoryPath = args[1];
             var directoryInfo = new DirectoryInfo(directoryPath);
-            var fileInfo = new FileInfo($"{directoryInfo.FullName}/identifierArchive.settings.txt");
+            var fileInfo = new FileInfo($"{directoryInfo.FullName}/identifierArchive.settings.json");
 
-            switch (subCommnad)
+            return subCommnad switch
             {
-                case SubCommands.CREATE:
+                SubCommands.CREATE => Cteate(directoryInfo, fileInfo),
+                SubCommands.READ => Read(directoryInfo, fileInfo),
+                _ => new ActionInfo()
+                {
+                    IsError = true,
+                    Message = "Unknown command."
+                },
+            };
+        }
 
-                    if (!directoryInfo.Exists)
-                    {
-                        return new ActionInfo()
-                        {
-                            IsError = true,
-                            Message = " Directory is not exists."
-                        };
-                    }
-
-                    if (fileInfo.Exists)
-                    {
-                        return new ActionInfo()
-                        {
-                            Message = $"File is exists. ({fileInfo.FullName})"
-                        };
-                    }
-                    File.WriteAllText(fileInfo.FullName, new SettingData().ToText());
-
-                    break;
-                
-                case SubCommands.READ:
-                    
-                    if (!directoryInfo.Exists)
-                    {
-                        return new ActionInfo()
-                        {
-                            IsError = true,
-                            Message = $" Directory is not exists. ({directoryInfo.FullName})"
-                        };
-                    }
-
-                    if (!fileInfo.Exists)
-                    {
-                        return new ActionInfo()
-                        {
-                            IsError = true,
-                            Message = $"File is not exists. ({fileInfo.FullName})"
-                        };
-                    }
-
-                    var text = File.ReadAllText(fileInfo.FullName);
-                    var data = SettingData.FromText(text);
-
-                    return new ActionInfo()
-                    {
-                        Message = $"Settings : \n{data.ToText()}".Replace("\n","\n\t"),
-                    };
-
-                default:
-                    return new ActionInfo()
-                    {
-                        IsError = true,
-                        Message = "Unknown command."
-                    };
+        private static ActionInfo Cteate(DirectoryInfo directoryInfo, FileInfo fileInfo)
+        {
+            if (!directoryInfo.Exists)
+            {
+                return new ActionInfo()
+                {
+                    IsError = true,
+                    Message = " Directory is not exists."
+                };
             }
 
-            return null;
+            if (!fileInfo.Exists)
+            {
+                File.WriteAllBytes(fileInfo.FullName, new SettingData().ToBytes());
+            }
+
+            var identifierFileInfo = new FileInfo($"{directoryInfo.FullName}/.identifier");
+            if (!identifierFileInfo.Exists)
+            {
+                File.WriteAllText(identifierFileInfo.FullName, "identifier");
+            }
+
+            var currentIdentifierFileInfo = new FileInfo($"{directoryInfo.FullName}/current.identifier");
+            if (!currentIdentifierFileInfo.Exists)
+            {
+                File.WriteAllText(currentIdentifierFileInfo.FullName, "current identifier");
+            }
+
+            var ignoreFileInfo = new FileInfo($"{directoryInfo.FullName}/.gitignore");
+            if (!ignoreFileInfo.Exists)
+            {
+                File.WriteAllText(ignoreFileInfo.FullName, "*\n!.gitignore\n!.identifier");
+            }
+
+            return new ActionInfo()
+            {
+                Message = $"Setting file is created. ({fileInfo.FullName})"
+            };
         }
+
+        private static ActionInfo Read(DirectoryInfo directoryInfo, FileInfo fileInfo)
+        {
+            if (!directoryInfo.Exists)
+            {
+                return new ActionInfo()
+                {
+                    IsError = true,
+                    Message = $" Directory is not exists. ({directoryInfo.FullName})"
+                };
+            }
+
+            if (!fileInfo.Exists)
+            {
+                return new ActionInfo()
+                {
+                    IsError = true,
+                    Message = $"File is not exists. ({fileInfo.FullName})"
+                };
+            }
+
+            var text = File.ReadAllBytes(fileInfo.FullName);
+            var data = SettingData.FromBytes(text);
+
+            return new ActionInfo()
+            {
+                Message = $"Settings : \n{JsonSerializer.PrettyPrint(data.ToBytes())}\n",
+            };
+        }
+
     }
 }
