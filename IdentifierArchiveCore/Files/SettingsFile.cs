2aa81e6cb07d2e856795d35762e40469e5e479c3
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2024-05-09 23:37:43 +0900

    SettingsFile.Controller を、 TargetFolderContoroller にリネーム

--- IdentifierArchiveCore/Files/SettingsFile.cs
+++ IdentifierArchiveCore/Files/SettingsFile.cs
@@ -46,7 +46,7 @@ namespace StudioIdGames.IdentifierArchiveCore.Files
         /// </summary>
         public string DownloadCommand { get; private set; } = $"\"GoogleDriveStrage.exe\" d {LOCALKEY_FOLDER_ABS}/.gdrivelocalkey~ %GoogleDriveStrage.RootFolderID% {ZIP_FILE.Parent} {ZIP_FILE.PathAbsolute}";
 
-        private void Replace(string var, string? value)
+        public void Replace(string var, string? value)
         {
             if (value != null)
             {
@@ -59,7 +59,7 @@ namespace StudioIdGames.IdentifierArchiveCore.Files
             }
         }
 
-        private void Replace<T>(PathIdentity var, PathIdentityInfo<T> value) where T : FileSystemInfo
+        public void Replace<T>(PathIdentity var, PathIdentityInfo<T> value) where T : FileSystemInfo
         {
             if (value != null)
             {
@@ -70,95 +70,6 @@ namespace StudioIdGames.IdentifierArchiveCore.Files
                 UploadCommand = value.Replace(UploadCommand, var);
                 DownloadCommand = value.Replace(DownloadCommand, var);
             }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="settingsFolderPath">実行ファイルから見た SettingsFile のあるフォルダーの場所</param>
-        /// <param name="targetFolderPath">SettingsFile から見た ターゲットフォルダーの場所</param>
-        public static Controller GetController(string settingsFolderPath, string targetFolderPath)
-        {
-            return new Controller(settingsFolderPath, targetFolderPath);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="settingsFolderPath">実行ファイルから見た SettingsFile のあるフォルダーの場所</param>
-        /// <param name="targetFolderPath">SettingsFile から見た ターゲットフォルダーの場所</param>
-        public class Controller(string settingsFolderPath, string targetFolderPath)
-        {
-            /// <summary>
-            /// 実行ファイルから見た SettingsFile の場所
-            /// </summary>
-            public string SettingsFolderPath { get; } = settingsFolderPath;
-
-            /// <summary>
-            /// SettingsFile から見た ターゲットフォルダーの場所
-            /// </summary>
-            public string? TargetFolderPath { get; } = targetFolderPath;
-
-            public SettingsFile Settings { get; private set; } = new();
-
-            public DirectoryInfo SettingsFolderInfo => new(SettingsFolderPath);
-
-            public FileInfo SettingsFileInfo => new($"{SettingsFolderPath}/{FileName}");
-
-            public DirectoryInfo LocalkeyFolderInfo => new(Settings.LocalkeyFolderAbsolute);
-
-            public FileInfo LocalkeyFileInfo => new($"{Settings.LocalkeyFolderAbsolute}/{LocalKeyFile.FileName}");
-
-            public PathIdentityInfo<FileInfo> ZipFileInfo => new(new(Settings.ZipFile), SettingsFolderInfo);
-
-            public PathIdentityInfo<DirectoryInfo> TargetFolderInfo => new(new($"{SettingsFolderPath}/{TargetFolderPath}"), SettingsFolderInfo);
-
-            public FileInfo IdentifierFileInfo => new($"{SettingsFolderPath}/{TargetFolderPath}/{IdentifierFile.ArchiveFileName}");
-
-            public FileInfo CurrentIdentifierFileInfo => new($"{SettingsFolderPath}/{TargetFolderPath}/{IdentifierFile.CurrentFileName}");
-
-            /// <summary>
-            /// 設定ファイルを読み込み、可能であれば環境変数を適用します。
-            /// </summary>
-            /// <param name="targetFolderPath">SettingsFile から見た ターゲットフォルダーの場所</param>
-            /// <param name="identifier">ターゲットとする識別子</param>
-            /// <returns></returns>
-            public bool TryLoad(string identifier)
-            {
-                bool res = false;
-                var settings = FromFile(SettingsFileInfo);
-                if (settings != null)
-                {
-                    Settings = settings;
-                    res = true;
-                }
-
-                Settings.Replace(IDENTIFIER, identifier);
-                Settings.Replace(TARGET_FOLDER, TargetFolderInfo);
-                Settings.Replace(SETTINGS_FOLDER_ABS, SettingsFolderInfo.FullName);
-                Settings.Replace(SETTINGS_FILE_ABS, SettingsFileInfo.FullName);
-                Settings.Replace(LOCALKEY_FOLDER_ABS, LocalkeyFolderInfo.FullName);
-                Settings.Replace(ZIP_FILE, ZipFileInfo);
-
-                var localkeyFile = LocalKeyFile.FromFile(LocalkeyFileInfo);
-                if (localkeyFile != null)
-                {
-                    foreach (var item in localkeyFile.KeySet)
-                    {
-                        Settings.Replace(item.Key, item.Value);
-                    }
-                }
-
-                return res;
-            }
-
-            public int ExcuteZip() => Utility.ExcuteCommand(Settings.ZipCommand);
-
-            public int ExcuteUnzip() => Utility.ExcuteCommand(Settings.UnzipCommand);
-
-            public int ExcuteUpload() => Utility.ExcuteCommand(Settings.UploadCommand);
-
-            public int ExcuteDownload() => Utility.ExcuteCommand(Settings.DownloadCommand);
         }
     }
 }
---
+++ IdentifierArchiveCore/Files/TargetFolderController.cs
@@ -0,0 +1,81 @@
+﻿namespace StudioIdGames.IdentifierArchiveCore.Files
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="settingsFolderPath">実行ファイルから見た SettingsFile のあるフォルダーの場所</param>
+    /// <param name="targetFolderPath">SettingsFile から見た ターゲットフォルダーの場所</param>
+    public class TargetFolderController(string settingsFolderPath, string? targetFolderPath)
+    {
+        /// <summary>
+        /// 実行ファイルから見た SettingsFile の場所
+        /// </summary>
+        public string SettingsFolderPath { get; } = settingsFolderPath;
+
+        /// <summary>
+        /// SettingsFile から見た ターゲットフォルダーの場所
+        /// </summary>
+        public string? TargetFolderPath { get; } = targetFolderPath;
+
+        public SettingsFile Settings { get; private set; } = new();
+
+        public DirectoryInfo SettingsFolderInfo => new(SettingsFolderPath);
+
+        public FileInfo SettingsFileInfo => new($"{SettingsFolderPath}/{SettingsFile.FileName}");
+
+        public DirectoryInfo LocalkeyFolderInfo => new(Settings.LocalkeyFolderAbsolute);
+
+        public FileInfo LocalkeyFileInfo => new($"{Settings.LocalkeyFolderAbsolute}/{LocalKeyFile.FileName}");
+
+        public PathIdentityInfo<FileInfo> ZipFileInfo => new(new(Settings.ZipFile), SettingsFolderInfo);
+
+        public PathIdentityInfo<DirectoryInfo> TargetFolderInfo => new(new($"{SettingsFolderPath}/{TargetFolderPath}"), SettingsFolderInfo);
+
+        public FileInfo IdentifierFileInfo => new($"{SettingsFolderPath}/{TargetFolderPath}/{IdentifierFile.ArchiveFileName}");
+
+        public FileInfo CurrentIdentifierFileInfo => new($"{SettingsFolderPath}/{TargetFolderPath}/{IdentifierFile.CurrentFileName}");
+
+        /// <summary>
+        /// 設定ファイルを読み込み、可能であれば環境変数を適用します。
+        /// </summary>
+        /// <param name="targetFolderPath">SettingsFile から見た ターゲットフォルダーの場所</param>
+        /// <param name="identifier">ターゲットとする識別子</param>
+        /// <returns></returns>
+        public bool TryLoad(string identifier)
+        {
+            bool res = false;
+            var settings = SettingsFile.FromFile(SettingsFileInfo);
+            if (settings != null)
+            {
+                Settings = settings;
+                res = true;
+            }
+
+            Settings.Replace(SettingsFile.IDENTIFIER, identifier);
+            Settings.Replace(SettingsFile.TARGET_FOLDER, TargetFolderInfo);
+            Settings.Replace(SettingsFile.SETTINGS_FOLDER_ABS, SettingsFolderInfo.FullName);
+            Settings.Replace(SettingsFile.SETTINGS_FILE_ABS, SettingsFileInfo.FullName);
+            Settings.Replace(SettingsFile.LOCALKEY_FOLDER_ABS, LocalkeyFolderInfo.FullName);
+            Settings.Replace(SettingsFile.ZIP_FILE, ZipFileInfo);
+
+            var localkeyFile = LocalKeyFile.FromFile(LocalkeyFileInfo);
+            if (localkeyFile != null)
+            {
+                foreach (var item in localkeyFile.KeySet)
+                {
+                    Settings.Replace(item.Key, item.Value);
+                }
+            }
+
+            return res;
+        }
+
+        public int ExcuteZip() => Utility.ExcuteCommand(Settings.ZipCommand);
+
+        public int ExcuteUnzip() => Utility.ExcuteCommand(Settings.UnzipCommand);
+
+        public int ExcuteUpload() => Utility.ExcuteCommand(Settings.UploadCommand);
+
+        public int ExcuteDownload() => Utility.ExcuteCommand(Settings.DownloadCommand);
+    }
+}
