8ff580eb5d276619016e0cc426dae1d8f2ed7e85
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2024-05-10 01:27:49 +0900

    保存ファイル名の修正

--- IdentifierArchiveCore/Files/IdentifierFile.cs
+++ IdentifierArchiveCore/Files/IdentifierFile.cs
@@ -2,8 +2,8 @@
 {
     public class IdentifierFile : TextFile
     {
-        public const string ArchiveFileName = "identifierArchive.identifier";
-        public const string CurrentFileName = "identifierArchiveCurrent.identifier";
+        public const string ArchiveFileName = "IdentifierArchive.identifier";
+        public const string CurrentFileName = "IdentifierArchiveCurrent.identifier";
         public const string DefaultValue = "DEFAULT_IDENTIFIER";
 
         public static bool ToFile(FileInfo fileInfo)
