616a86d2df4aeb16f47c4178d7244fc4ff75c5cc
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2024-05-10 22:31:13 +0900

    7 update file system (#9)
    * FileSystemの抽象化、用語の統一など
    
    * SettingsFile.Controller を、 TargetFolderContoroller にリネーム
    
    * デフォルト設定の変更
    
    * 保存ファイル名の修正
    
    * 既存コードを新しいファイルシステムに対応させる
    
    * fix invailed code
    
    * fix bug
    
    * リファクタリング

--- IdentifierArchiveCore/Files/SettingsFile.cs
+++ IdentifierArchiveCore/Files/SettingsFile.cs
@@ -1,65 +1,75 @@
-﻿using Utf8Json;
+﻿using System.Globalization;
+using System.IO.Compression;
+using System.Reflection;
+using Utf8Json;
 
 namespace StudioIdGames.IdentifierArchiveCore.Files
 {
-    public static class SettingsFile
+    public class SettingsFile : JsonFile<SettingsFile>
     {
-        public const string SETTINGS_PATH = "%SETTINGS_PATH%";
-        public const string TARGET_PATH = "%TARGET_PATH%";
-        public const string TARGET_NAME = "%TARGET_NAME%";
-        public const string IDENTIFIER = "%IDENTIFIER%";
-        public const string WORK_SPACE = "%WORK_SPACE%";
-        public const string ZIP_PATH = "%ZIP_PATH%";
+        public const string IDENTIFIER = $"%{nameof(IDENTIFIER)}%";
+        public static readonly string SETTINGS_FILE_ABS = $"%{nameof(SETTINGS_FILE_ABS)}%";
+        public static readonly string SETTINGS_FOLDER_ABS = $"%{nameof(SETTINGS_FOLDER_ABS)}%";
+        public static readonly string LOCALKEY_FOLDER_ABS = $"%{nameof(LOCALKEY_FOLDER_ABS)}%";
+        public static readonly string ZIP_FILE_ABS = $"%{nameof(ZIP_FILE_ABS)}%";
+        public static readonly string ZIP_FOLDER_ABS = $"%{nameof(ZIP_FOLDER_ABS)}%";
+        public static readonly PathIdentity TARGET_FOLDER = new(nameof(TARGET_FOLDER));
 
-        public const string FileName = "identifierArchiveSettings.json";
+        public const string FileName = "IdentifierArchiveSettings.json";
 
-        public static byte[] DefaultValue { get; } = new Data().ToBytes();
+        /// <summary>
+        /// 保安上隠蔽したいファイルを配置するためのフォルダを表す絶対パス
+        /// </summary>
+        public string LocalkeyFolderAbsolute { get; set; } = $"D:/LocalKeys~/ProjectName";
 
-        public static Data FromBytes(byte[] bytes)
+        /// <summary>
+        /// %TARGET_FOLDER% と 識別子 %IDENTIFIER% に対応する圧縮ファイルを表す、%SETTINGS_FILE_PARENT% からの相対ファイルパス
+        /// </summary>
+        public string ZipFile { get; set; } = $"{TARGET_FOLDER.Path}.ZipArchives~/{IDENTIFIER}.7z";
+
+        /// <summary>
+        /// %TARGET_FOLDER_ABS% 内の .identifier ファイル以外を圧縮して %ZIP_FILE_ABS% として保存するコマンド 
+        /// </summary>
+        public string ZipCommand { get;set; } = $"C:\\Program Files\\7-Zip\\7z.exe a -uq0 -p%MyPassWord% -xr!*.identifier {ZIP_FILE_ABS} {TARGET_FOLDER.PathAbsolute}";
+
+        /// <summary>
+        /// %ZIP_FILE_ABS% を解凍して %TARGET_FOLDER_ABS% に配置するコマンド 
+        /// </summary>
+        public string UnzipCommand { get; set; } = $"C:\\Program Files\\7-Zip\\7z.exe x -p%MyPassWord% {ZIP_FILE_ABS} -o{TARGET_FOLDER.PathAbsolute}";
+
+        /// <summary>
+        /// %ZIP_FILE_ABS% を、%ZIP_FILE_PARENT% から一意に定まるクラウドストレージにアップロードするコマンド
+        /// </summary>
+        public string UploadCommand { get; set; } = $"\"GoogleDriveStrage.exe\" u {LOCALKEY_FOLDER_ABS}/.gdrivelocalkey~ %GoogleDriveStrage.RootFolderID% {ZIP_FOLDER_ABS} {ZIP_FILE_ABS}";
+
+        /// <summary>
+        /// %ZIP_FILE_ABS% を、%ZIP_FILE_PARENT% から一意に定まるクラウドストレージからダウンロードするコマンド
+        /// </summary>
+        public string DownloadCommand { get;set; } = $"\"GoogleDriveStrage.exe\" d {LOCALKEY_FOLDER_ABS}/.gdrivelocalkey~ %GoogleDriveStrage.RootFolderID% {ZIP_FOLDER_ABS} {ZIP_FILE_ABS}";
+
+        public void Replace(string var, string? value)
         {
-            return JsonSerializer.Deserialize<Data>(bytes);
+            if (value != null)
+            {
+                LocalkeyFolderAbsolute = LocalkeyFolderAbsolute.Replace(var, value);
+                ZipFile = ZipFile.Replace(var, value);
+                ZipCommand = ZipCommand.Replace(var, value);
+                UnzipCommand = UnzipCommand.Replace(var, value);
+                UploadCommand = UploadCommand.Replace(var, value);
+                DownloadCommand = DownloadCommand.Replace(var, value);
+            }
         }
 
-        public class Data
+        public void Replace<T>(PathIdentity var, PathIdentityInfo<T> value) where T : FileSystemInfo
         {
-            public string TargetBasePath { get; set; } = "./";
-            public string ZipPath { get; set; } = $"{TARGET_PATH}.ZipArchives~/{IDENTIFIER}.7z";
-            public string ZipCommand { get; set; } = $"C:\\Program Files\\7-Zip\\7z.exe a -uq0 -p%MyPassWord% -xr!*.identifier {ZIP_PATH} {TARGET_PATH}";
-            public string UnzipCommand { get; set; } = $"C:\\Program Files\\7-Zip\\7z.exe x -p%MyPassWord% {ZIP_PATH} -o{TARGET_PATH}";
-            public string UploadCommand { get; set; } = $"\"GoogleDriveStrage.exe\" u {SETTINGS_PATH}.localkeygdrive~ %GoogleDriveStrage.RootFolderID% {TARGET_NAME} {ZIP_PATH}";
-            public string DownloadCommand { get; set; } = $"\"GoogleDriveStrage.exe\" d {SETTINGS_PATH}.localkeygdrive~ %GoogleDriveStrage.RootFolderID% {TARGET_NAME} {ZIP_PATH}";
-
-            public byte[] ToBytes()
+            if (value != null)
             {
-                return JsonSerializer.PrettyPrintByteArray(JsonSerializer.Serialize(this));
-            }
-
-            public void SetEnvironment(string? targetName = null, string? identifier = null, string? settingsPath = null, LocalKeyFile.Data? localKey = null)
-            {
-                if (localKey != null)
-                {
-                    foreach (var key in localKey.KeySet)
-                    {
-                        Replace(key.Key, key.Value);
-                    }
-                }
-                Replace(SETTINGS_PATH, settingsPath);
-                Replace(TARGET_PATH, new DirectoryInfo($"{TargetBasePath.TrimEnd('/', '\\')}/{targetName}").FullName);
-                Replace(TARGET_NAME, targetName);
-                Replace(IDENTIFIER, identifier);
-                Replace(ZIP_PATH, new DirectoryInfo(ZipPath).FullName);
-            }
-
-            private void Replace(string var, string? value)
-            {
-                if (value != null)
-                {
-                    ZipPath = ZipPath.Replace(var, value);
-                    ZipCommand = ZipCommand.Replace(var, value);
-                    UnzipCommand = UnzipCommand.Replace(var, value);
-                    UploadCommand = UploadCommand.Replace(var, value);
-                    DownloadCommand = DownloadCommand.Replace(var, value);
-                }
+                LocalkeyFolderAbsolute = value.Replace(LocalkeyFolderAbsolute, var);
+                ZipFile = value.Replace(ZipFile, var);
+                ZipCommand = value.Replace(ZipCommand, var);
+                UnzipCommand = value.Replace(UnzipCommand, var);
+                UploadCommand = value.Replace(UploadCommand, var);
+                DownloadCommand = value.Replace(DownloadCommand, var);
             }
         }
     }
---
+++ IdentifierArchiveCore/Files/TargetFolderController.cs
@@ -0,0 +1,143 @@
+﻿namespace StudioIdGames.IdentifierArchiveCore.Files
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="settingsFolderPath">実行ファイルから見た SettingsFile のあるフォルダーの場所</param>
+    /// <param name="targetFolderPath">SettingsFile から見た ターゲットフォルダーの場所</param>
+    public class TargetFolderController(string settingsFolderPath, string targetFolderPath)
+    {
+        /// <summary>
+        /// 実行ファイルから見た SettingsFile の場所
+        /// </summary>
+        public string SettingsFolderPath { get; } = settingsFolderPath;
+
+        /// <summary>
+        /// SettingsFile から見た ターゲットフォルダーの場所
+        /// </summary>
+        public string TargetFolderPath { get; } = targetFolderPath;
+
+        public SettingsFile Settings { get; private set; } = new();
+
+        public DirectoryInfo SettingsFolderInfo => new(SettingsFolderPath);
+
+        public FileInfo SettingsFileInfo => new($"{SettingsFolderPath}/{SettingsFile.FileName}");
+
+        public DirectoryInfo LocalkeyFolderInfo => new(Settings.LocalkeyFolderAbsolute);
+
+        public FileInfo LocalkeyFileInfo => new($"{Settings.LocalkeyFolderAbsolute}/{LocalKeyFile.FileName}");
+
+        public FileInfo ZipFileInfo => new(Settings.ZipFile);
+        public DirectoryInfo ZipFolderInfo => ZipFileInfo.Directory!;
+
+        public PathIdentityInfo<DirectoryInfo> TargetFolderInfo => new(new($"{SettingsFolderPath}/{TargetFolderPath}"), SettingsFolderInfo);
+
+        public FileInfo IdentifierFileInfo => new($"{SettingsFolderPath}/{TargetFolderPath}/{IdentifierFile.ArchiveFileName}");
+
+        public FileInfo CurrentIdentifierFileInfo => new($"{SettingsFolderPath}/{TargetFolderPath}/{IdentifierFile.CurrentFileName}");
+
+        public FileInfo TargetGitIgnoreFileInfo => new($"{SettingsFolderPath}/{TargetFolderPath}/{GitIgnoreFile.FileName}");
+
+        public ActionInfo? CheckSettingsFileAndTargetFolder()
+        {
+            if (!SettingsFileInfo.Exists)
+            {
+                return new ActionInfo()
+                {
+                    IsError = true,
+                    Message = $"Settings File が見つかりません。({SettingsFileInfo.FullName})"
+                };
+            }
+
+            if (!TargetFolderInfo.Info.Exists)
+            {
+                return new ActionInfo()
+                {
+                    IsError = true,
+                    Message = $"Target Folder が見つかりません。({TargetFolderInfo.Info.FullName})"
+                };
+            }
+
+            return null;
+        }
+
+        public void SetupTargetFolder()
+        {
+            if (!IdentifierFileInfo.Exists)
+            {
+                IdentifierFile.ToFile(IdentifierFileInfo);
+            }
+
+            if (!CurrentIdentifierFileInfo.Exists)
+            {
+                IdentifierFile.ToFile(CurrentIdentifierFileInfo);
+            }
+
+            if (!TargetGitIgnoreFileInfo.Exists)
+            {
+                GitIgnoreFile.ToFileInTargetFolder(TargetGitIgnoreFileInfo);
+            }
+        }
+
+        public void SetupZipFolder()
+        {
+            if (!ZipFolderInfo.Exists)
+            {
+                ZipFolderInfo.Create();
+            }
+
+            var zipIgnoreFileInfo = new FileInfo($"{ZipFolderInfo.FullName}/{GitIgnoreFile.FileName}");
+            if (!zipIgnoreFileInfo.Exists)
+            {
+                TextFile.ToFile(zipIgnoreFileInfo, $"*\n!{GitIgnoreFile.FileName}");
+            }
+        }
+
+        /// <summary>
+        /// 設定ファイルを読み込み、可能であれば環境変数を適用します。
+        /// </summary>
+        /// <param name="targetFolderPath">SettingsFile から見た ターゲットフォルダーの場所</param>
+        /// <param name="identifier">ターゲットとする識別子</param>
+        /// <returns></returns>
+        public bool TryLoad(string identifier, bool ignoreLocalkey = false)
+        {
+            bool res = false;
+            var settings = SettingsFile.FromFile(SettingsFileInfo);
+            if (settings != null)
+            {
+                Settings = settings;
+                res = true;
+            }
+
+            Settings.Replace(SettingsFile.IDENTIFIER, identifier);
+            Settings.Replace(SettingsFile.TARGET_FOLDER, TargetFolderInfo);
+            Settings.Replace(SettingsFile.SETTINGS_FOLDER_ABS, SettingsFolderInfo.FullName);
+            Settings.Replace(SettingsFile.SETTINGS_FILE_ABS, SettingsFileInfo.FullName);
+            Settings.Replace(SettingsFile.ZIP_FILE_ABS, ZipFileInfo.FullName);
+            Settings.Replace(SettingsFile.ZIP_FOLDER_ABS, ZipFolderInfo.FullName);
+            Settings.Replace(SettingsFile.LOCALKEY_FOLDER_ABS, LocalkeyFolderInfo.FullName);
+
+            if (!ignoreLocalkey)
+            {
+                var localkeyFile = LocalKeyFile.FromFile(LocalkeyFileInfo);
+                if (localkeyFile != null)
+                {
+                    foreach (var item in localkeyFile.KeySet)
+                    {
+                        Settings.Replace(item.Key, item.Value);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public int ExcuteZip() => Utility.ExcuteCommand(Settings.ZipCommand);
+
+        public int ExcuteUnzip() => Utility.ExcuteCommand(Settings.UnzipCommand);
+
+        public int ExcuteUpload() => Utility.ExcuteCommand(Settings.UploadCommand);
+
+        public int ExcuteDownload() => Utility.ExcuteCommand(Settings.DownloadCommand);
+    }
+}
