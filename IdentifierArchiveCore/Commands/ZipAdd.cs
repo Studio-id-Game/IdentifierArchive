efb711e25443dcc0305cc5321bbcf9a0504241f8
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2024-05-24 17:55:56 +0900

    ZipAdd の実装

--- IdentifierArchiveCore/Commands/ZipAdd.cs
+++ IdentifierArchiveCore/Commands/ZipAdd.cs
@@ -1,18 +1,88 @@
-﻿namespace StudioIdGames.IdentifierArchiveCore.Commands
+﻿using StudioIdGames.IdentifierArchiveCore.Files;
+using StudioIdGames.IdentifierArchiveCore.FolderControllers;
+using System.IO;
+
+namespace StudioIdGames.IdentifierArchiveCore.Commands
 {
-    public class ZipAdd : ICommandAction
+    public class ZipAdd : CommandAction
     {
-        public const string CommandID = "za";
+        public static ZipAdd Instance { get; } = new ZipAdd();
 
-        public const string Name = "Zip-Add";
+        private ZipAdd() { }
 
-        string ICommandAction.CommandID => CommandID;
+        public override string CommandID => "za";
 
-        string ICommandAction.Name => Name;
+        public override string Name => "Zip-Add";
 
-        public int Excute(CommandArgs args)
+        public override int Excute(CommandArgs args)
         {
-            throw new NotImplementedException();
+            base.Excute(args);
+
+            if (!args.CheckRequire(this, settingsFodler: true, targetFolder: true))
+            {
+                return -1;
+            }
+
+            var settingsFolderController = new SettingsFolderController(args.SettingsFolder);
+            var targetFolderController = new TargetFolderController(settingsFolderController, args.TargetFolder);
+            var archiveIdentifier = targetFolderController.GetArchiveIdentifier(true) ?? new IdentifierFile(IdentifierFileType.Archive);
+
+            if (!string.IsNullOrWhiteSpace(args.Identifier))
+            {
+                archiveIdentifier.Text = args.Identifier;
+                archiveIdentifier.FixIdentifier(0);
+            }
+
+            var settings = settingsFolderController.GetSettingsFile(out _, out var safe, targetFolderController.FolderInfo, archiveIdentifier.Text);
+
+            if (settings == null)
+            {
+                return -1;
+            }
+
+            var zipFileInfo = settings.GetZipFileInfo();
+            
+            while (zipFileInfo.Exists)
+            {
+                Console.WriteLine($"{archiveIdentifier.Text} exists. ({zipFileInfo.FullName})");
+                archiveIdentifier.FixIdentifier(1);
+
+                settings = settingsFolderController.GetSettingsFile(out _, out safe, targetFolderController.FolderInfo, archiveIdentifier.Text);
+                
+                if (settings == null)
+                {
+                    return -1;
+                }
+
+                zipFileInfo = settings.GetZipFileInfo();
+            }
+
+            Console.WriteLine($"Create zip file. ({zipFileInfo.FullName})");
+
+            Console.WriteLine($"Excute Zip Command :\n{safe?.ZipCommand}\n");
+
+            var ret = settings.ExcuteZip();
+
+            Console.WriteLine();
+
+            if(ret < 0)
+            {
+                Console.WriteLine("Zip Command failed.\n");
+                return ret;
+            }
+
+            Console.WriteLine("Zip Command complete.\n");
+
+            Console.WriteLine("Update Identifier files...\n");
+
+            if (!targetFolderController.SetIdentifier(archiveIdentifier.Text))
+            {
+                return -1;
+            }
+
+            Console.WriteLine("Updated Identifier files.\n");
+
+            return 0;
         }
     }
 }
