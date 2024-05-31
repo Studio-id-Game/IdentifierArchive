42d0cf3c0edc17a911e3652caef4d99c333ba7fe
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2024-05-31 22:23:08 +0900

    ビルド設定をLinuxに対応させる  (#30)
    * update for linux
    
    * update postbuild
    
    * Create build_linux.sh1
    
    * ps の統合
    
    * Update PostBuild
    
    * Update build_linux.sh1
    
    * bug fix

--- IdentifierArchiveCore/post.ps1
+++ IdentifierArchiveCore/post.ps1
@@ -1,2 +1,12 @@
-echo "copy $Args[0] to $Args[1]"
-cp -r -Force -v $args[0] $args[1]
+param($TargetDir, $ProjectDir, $TargetName)
+
+$TargetDir = $($TargetDir).Replace("\","/");
+$ProjectDir = $($ProjectDir).Replace("\","/");
+$TargetName = $($TargetName).Replace("\","/");
+$CopyTo = "$($ProjectDir)/../$($TargetName)"
+
+echo "New-Item "$($CopyTo)" -ItemType Directory -ErrorAction SilentlyContinue"
+New-Item "$($CopyTo)" -ItemType Directory -ErrorAction SilentlyContinue
+
+echo "Copy-Item -Path '$($TargetDir)'/* -Destination '$($CopyTo)' -Force -Recurse"
+Copy-Item -Path "$($TargetDir)/*" -Destination "$($CopyTo)" -Force -Recurse 
